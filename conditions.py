3b28ea7d94bd92d4e68b1a298a58e70c68bcb13f
watchout254 <mukenyadaniel299@gmail.com>
2024-03-17 11:19:39 +0300

    Hackathon my guy

--- conditions.py
+++ conditions.py
@@ -9,3 +9,11 @@
 # - Prompts a user to enter their age.
 # - Uses a conditional statement to check if the age is greater than or equal to 18.
 # - Prints "You are eligible to vote" if true, otherwise "You are not eligible to vote."
+print("Very warm welcome to Mukenyas Registration Voters card")
+
+age = int(input("Kindly enter your age: "));
+
+if age >= 18:
+    print("Wonderful, You are eligible to vote");
+else:
+    print("Oooops!!Sorry you can't vote my guy,wait till you reach 18 years");
